f1c7512df4755266a1cd424ee7b4bbbb6b7d4231
mustafa-bilen <mustafabilen6669@gmail.com>
2022-10-24 14:35:58 +0300

    random-app

---
+++ random-user-app-youtube/src/components/Header.jsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
+import { HiLocationMarker } from "react-icons/hi";
+
+const Header = () => {
+  const [data, setData] = useState(null);
+
+  const getApi = () => {
+    axios
+      .get("https://randomuser.me/api/")
+      .then((res) => {
+        setData(res.data.results[0]);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  return (
+    <div className="w-full min-h-screen flex flex-col justify-center items-center">
+      <div className="bos">
+        {data ? (
+          <div className="flex justify-between items-center flex-col w-[30rem] h-[30rem] mx-auto text-center bg-blue-300 p-4">
+            <img
+              src={data.picture.large}
+              alt={data.name.first}
+              className="rounded-full w-[9rem]"
+            />
+            <p className="text-2xl">
+              {data.name.title} {data.name.first} {data.name.last}
+            </p>
+            <p className="flex items-center">
+              <AiOutlineMail className="text-2xl mr-2" /> {data.email}
+            </p>
+            <p className="flex items-center">
+              <AiOutlinePhone className="text-2xl mr-2 " /> {data.phone}
+            </p>
+            <p className="flex items-center">
+              <HiLocationMarker className="text-2xl mr-2" />{" "}
+              {data.location.city}-{data.location.country}
+            </p>
+            <p>Age: {data.dob.age}</p>
+            <p>Register Date: {data.registered.date.slice(0, 10)}</p>
+          </div>
+        ) : (
+          "Data not found"
+        )}
+      </div>
+      <button
+        className="bg-blue-500 p-1 text-white hover:bg-slate-400 hover:text-black mt-5"
+        onClick={() => getApi()}
+      >
+        Random User
+      </button>
+    </div>
+  );
+};
+
+export default Header;
